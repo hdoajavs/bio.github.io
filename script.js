96279b371a491c6d69c356a35fc0d8ee9615f2f9
hdoajavs <141851559+hdoajavs@users.noreply.github.com>
2025-02-14 22:30:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Функция для открытия/закрытия меню
+const menuButton = document.getElementById('menuButton');
+const menu = document.getElementById('menu');
 
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle('active'); // Добавляем/удаляем класс active
+});
+
+// Закрытие меню при клике вне его
+document.addEventListener('click', (event) => {
+    if (!menu.contains(event.target) && event.target !== menuButton) {
+        menu.classList.remove('active');
+    }
+});
